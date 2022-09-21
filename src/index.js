859204cf9ee36484d1a05651e4736ae7e5db12d1
Renato Macedo <renato_macedo_1@hotmail.com>
2022-09-21 11:11:11 +0100

    Added Auth0

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-u5uhn850.us.auth0.com"
+      clientId="d9LvAtvjNHepCxlYRCFOp9OGZRD8APoQ"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
